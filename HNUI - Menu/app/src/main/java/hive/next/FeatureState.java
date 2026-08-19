1eb727d987dd861b8668ab8f9957931409fafa7d
ChenQing <JisGreen@163.com>
2026-08-19 14:13:07 +0800

    Delete HNUI - Menu directory

--- HNUI - Menu/app/src/main/java/hive/next/FeatureState.java
+++
@@ -1,8 +0,0 @@
-package hive.next;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class FeatureState {
-    public static final List<String> activeFeatures = new ArrayList<String>();
-}
